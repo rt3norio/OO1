9a8fc4aafca721ae2ef9c7bb8c44378f5b832d01
Rodrigo Tenorio <rt3norio@gmail.com>
2016-03-22 18:31:36 -0300

    Update CavalinhoDoBozo.java

--- CavalinhoDoBozo.java
+++ CavalinhoDoBozo.java
@@ -5,29 +5,36 @@ public class CavalinhoDoBozo {
 	
 	 public static void main(String args[]){
 
-		 Cavalo [] c = new Cavalo[3];
+		 Cavalo [] c = new Cavalo[5];
 		 c[0] = new Cavalo("BRANQUINHO");
 		 c[1] = new Cavalo("MALHADINHO");
 		 c[2] = new Cavalo("PRETINHO");
+		 c[3] = new Cavalo("YURI        ");
+		 c[4] = new Cavalo("MAURICINHO");
 	 
 		 while (true){
-			 System.out.println("                                                                           \\/FIM!");
+			 System.out.println("                                                                              \\/FIM!");
 			 for (Cavalo atual:c){
-				 if (atual.distanciaAtual >= 60.0){
-					 System.out.println(atual.nome+" Ganhou!");
-					 return;
-				 }
 				 atual.Correr();
 				 atual.Mostrar();
-				 
+				 if (atual.distanciaAtual >= 60.0){
+					 System.out.println("\n\n\n\n\n\n\n\n\n");
+					 System.out.println("                                                                              \\/FIM!");
+					 for (Cavalo atualFim:c){
+						 atualFim.Mostrar();
+					 }
+					 System.out.print(atual.nome+" Ganhou!");
+					 return;
+				 }
 			 }
+			 
 			 try {
 				    //thread to sleep for the specified number of milliseconds
-				    Thread.sleep(100);
+				    Thread.sleep(50);
 				} catch ( java.lang.InterruptedException ie) {
 				    System.out.println(ie);
 				}
-			 System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+			 System.out.println("\n\n\n\n\n\n\n\n\n");
 
 		 }
 	}
@@ -35,7 +42,7 @@ public class CavalinhoDoBozo {
 
 class Cavalo{
 	String nome;
-	String[] nomesPossiveis = {"BRANQUINHO","MALHADINHO","PRETINHO"};
+	String[] nomesPossiveis = {"BRANQUINHO","MALHADINHO","PRETINHO","YURI"};
 	String icone;
 	Double distanciaAtual;
 
@@ -55,17 +62,17 @@ class Cavalo{
 		this.distanciaAtual = 0.0;
 	}
 
-	/*  2) a correr, que sorteia um número real entre 0 e 1, acumula esse valor a
-	 *  sua distância percorrida (número real) e chama a função privada mostrar do objeto,
-	 *  e retorna a distância atual já percorrida pelo cavalo; */
+	/*  2) a correr, que sorteia um nÃºmero real entre 0 e 1, acumula esse valor a
+	 *  sua distÃ¢ncia percorrida (nÃºmero real) e chama a funÃ§Ã£o privada mostrar do objeto,
+	 *  e retorna a distÃ¢ncia atual jÃ¡ percorrida pelo cavalo; */
 	public void Correr(){
 		this.distanciaAtual += Math.random();
 		
 	}
-	/*3) o método privado mostrar, que exibe no console um linha com o nome do cavalo e,
-	 *  ao lado, o caractere avatar dele, distante n caracteres da posição do dois pontos,
-	 *   sendo n a parte inteira da distância percorrida. Considerando a distância zero,
-	 *    a situação inicial da tela será a seguinte:
+	/*3) o mÃ©todo privado mostrar, que exibe no console um linha com o nome do cavalo e,
+	 *  ao lado, o caractere avatar dele, distante n caracteres da posiÃ§Ã£o do dois pontos,
+	 *   sendo n a parte inteira da distÃ¢ncia percorrida. Considerando a distÃ¢ncia zero,
+	 *    a situaÃ§Ã£o inicial da tela serÃ¡ a seguinte:
 	 *    BRANQUINHO: B>
 	 *    MALHADINHO: M>
 	 *    PRETINHO: P>
