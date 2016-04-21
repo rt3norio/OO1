fa7dda2b758adfa0822ef6b8ae6543a6620c5bb3
Rodrigo Tenorio <rt3norio@gmail.com>
2016-04-21 17:22:27 -0300

    criacao da classe bingo extendendo super()

---
+++ Orientados/bingo/src/main/java/br/com/bingo/Bingo.java
@@ -0,0 +1,24 @@
+package br.com.bingo;
+
+public class Bingo extends BingoAbstrato{
+
+	@Override
+	public boolean adicionarParticipante(int pos, String nome) {
+		
+		
+		return false;
+	}
+
+	@Override
+	public int[] getDisponiveis() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int sortear() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
