99803e2d569e8ccb47099787cb232eb2d5ab6706
Rodrigo Tenorio <rt3norio@gmail.com>
2016-04-21 16:17:30 -0300

    inicio, classe funcionario e metodos get e set
    incio

---
+++ T4/FuncExc/src/br/com/FuncExc/Funcionario.java
@@ -0,0 +1,31 @@
+package br.com.FuncExc;
+
+public class Funcionario {
+	private Integer matricula;
+	private String nome;
+	private Double salario;
+	
+	public Funcionario(String nome,Double salario){
+		
+		
+		
+	}
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Double getSalario() {
+		return salario;
+	}
+	public void setSalario(Double salario) {
+		this.salario = salario;
+	}
+	public Integer getMatricula() {
+		return matricula;
+	}
+	
+}
